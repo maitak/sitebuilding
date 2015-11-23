3454bc1518632f65724f0bcb49e3a279a1361524
Bastian Widmer <b.widmer@dasrecht.net>
2015-11-23 14:50:59 +0100

    Lowering Cachetime
    The value is (unlike in d7) not propagated to the backend so there could be a different value leading to issues.

--- sites/default/settings.production.php
+++ sites/default/settings.production.php
@@ -22,6 +22,6 @@ $settings['trusted_host_patterns'] = array(
 );
 
 # On production, we enable the cache and the css and js aggregation.
-$config['system.performance']['cache']['page']['max_age'] = 86400;
+$config['system.performance']['cache']['page']['max_age'] = 3600;
 $config['system.performance']['css']['preprocess'] = 1;
 $config['system.performance']['js']['preprocess'] = 1;
