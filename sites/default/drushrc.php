3e9ec662cbbd5e3a7bca19501206d51a0f7494d5
Bastian Widmer <b.widmer@dasrecht.net>
2015-12-10 15:13:26 +0100

    DEVOPS-376 - Disabling the sql-sync options to solve drush issues

--- sites/default/drushrc.php
+++ sites/default/drushrc.php
@@ -5,5 +5,5 @@ if (getenv('AMAZEEIO_SITE_URL')) {
 }
 $options['package-handler'] = 'git_drupalorg';
 $options['gitsubmodule'] = TRUE;
-$command_specific['sql-sync'] = array('no-cache' => TRUE, 'no-ordered-dump' => TRUE, 'create-db' => FALSE);
+# $command_specific['sql-sync'] = array('no-cache' => TRUE, 'no-ordered-dump' => TRUE, 'create-db' => FALSE);
 $command_specific['rsync'] = array('verbose' => TRUE);
