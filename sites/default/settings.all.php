ff0ce34a8c737ab82e81881889ac9dc818db33b9
Bastian Widmer <b.widmer@dasrecht.net>
2015-12-28 21:50:25 +0100

    Update settings.all.php

--- sites/default/settings.all.php
+++ sites/default/settings.all.php
@@ -6,6 +6,6 @@
  * This file should contain all settings.php configurations that are needed by all environments.
  */
 
-$settings['hash_salt'] = 'a9bbce32d98d9c9cfd1ea285b1fadeabc997b07ff956f9ac25ead8a607ef7d8ca30ad57ce3e8245fe17e68e2e76f8fa5d3d693d0ad8b8e973e5ff55d0ea6d84c';
+$settings['hash_salt'] = 'CHANGEME';
 
 $config_directories['sync'] = 'sites/default/config/sync';
